db535e6cae390c4b2f38d1e59ef723d2229d8809
Tanvir <tanvir.ahmeed.aiub@gmail.com>
2022-02-23 13:23:06 +0600

    Authorization

---
+++ Assoc/Assoc/Auth/AdminAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assoc.Auth
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AdminAccess : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var authenticated = base.AuthorizeCore(httpContext);
+            if (!authenticated) { 
+                return false; 
+            }
+            if (httpContext.Session["role"].ToString().Equals("Admin")){
+                return true;
+            }
+            return false;
+            //return base.AuthorizeCore(httpContext);
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.HttpContext.Response.Redirect("/home/contactc");
+         
+        }
+    }
+}
--- Assoc/Assoc/Controllers/HomeController.cs
+++ Assoc/Assoc/Controllers/HomeController.cs
@@ -1,17 +1,23 @@
-﻿using Assoc.Models.Database;
+﻿using Assoc.Auth;
+using Assoc.Models.Database;
 using Assoc.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Assoc.Controllers
 {
+    
     public class HomeController : Controller
     {
+        [Authorize]
+        //[AllowAnonymous]
         public ActionResult Index()
         {
+            
             UMS_Sp22_AEntities db = new UMS_Sp22_AEntities();
             var dept = (from d in db.Departments where d.Id == 1 select d).FirstOrDefault();
             DepartmentModel de = new DepartmentModel();
@@ -19,12 +25,46 @@ namespace Assoc.Controllers
             de.Id = dept.Id;
             return View(dept);
         }
+        [HttpGet]
+        public ActionResult Login() {
+            if (User.Identity.IsAuthenticated) { return RedirectToAction("Index"); }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            UMS_Sp22_AEntities db = new UMS_Sp22_AEntities();
+            var data = (from e in db.Users
+                        where e.Password.Equals(user.Password) &&
+                        e.Username.Equals(user.Username)
+                        select e).FirstOrDefault();
+            if (data != null) {
+                FormsAuthentication.SetAuthCookie(data.Username, false);
+                Session["role"] = data.Role;
+                return RedirectToAction("Index");
+            }
+
+            TempData["msg"] = "Invalid Crdentials";
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout() {
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+        [AdminAccess]
+        public ActionResult UserList() {
+
+            UMS_Sp22_AEntities db = new UMS_Sp22_AEntities();
+            return View(db.Users.ToList());
         }
 
         public ActionResult Contact()
